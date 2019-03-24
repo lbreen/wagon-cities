b54b294410cfa3e92f3d6d2df9239e0788836819
Luke Breen <l.breen1@outlook.com>
2019-03-24 16:56:39 +0200

    refactored activeCity component

--- src/components/active_city.jsx
+++ src/components/active_city.jsx
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-class ActiveCity extends Component {
-  render () {
-    return (
-      <div className="active-city">
-        <h2>{this.props.selectedCity.name}</h2>
-        <p>{this.props.selectedCity.address}</p>
-      </div>
-    );
-  }
-}
+const ActiveCity = ({ selectedCity: { name, address } }) => {
+  return (
+    <div className="active-city">
+      <h2>{name}</h2>
+      <p>{address}</p>
+    </div>
+  );
+};
+
 
 function mapStateToProps(state) {
   return { selectedCity: state.selectedCity };
